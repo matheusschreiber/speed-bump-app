73fe8808d6b7ed81bb1e496115a37c5ef56a7a63
Matheus <mt.schreiber01@gmail.com>
2023-04-29 17:04:58 -0300

    :wrench: refactor code and fixing issues on uploaded image detector

--- app/src/main/java/org/tensorflow/lite/examples/objectdetection/PhotoActivity.kt
+++ app/src/main/java/org/tensorflow/lite/examples/objectdetection/PhotoActivity.kt
@@ -14,10 +14,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import org.tensorflow.lite.examples.objectdetection.databinding.ActivityPhotoBinding
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
 
 class PhotoActivity : AppCompatActivity(), OnSignDetectedAlert {
 
     private lateinit var activityStartBinding: ActivityPhotoBinding
+    data class DetectionResult(val boundingBox: RectF, val text: String)
 
     var pickedPhoto : Uri? = null
     var pickedBitMap : Bitmap? = null
@@ -71,18 +74,18 @@ class PhotoActivity : AppCompatActivity(), OnSignDetectedAlert {
                     val source = ImageDecoder.createSource(this.contentResolver, pickedPhoto!!)
                     pickedBitMap = ImageDecoder.decodeBitmap(source)
                     galleryPhoto.setImageBitmap(pickedBitMap)
+                    runObjectDetection(pickedBitMap!!, galleryPhoto)
                     galleryText.setVisibility(View.INVISIBLE)
                     galleryButton.setVisibility(View.INVISIBLE)
                 }
                 else {
                     pickedBitMap = MediaStore.Images.Media.getBitmap(this.contentResolver,pickedPhoto)
                     galleryPhoto.setImageBitmap(pickedBitMap)
+                    runObjectDetection(pickedBitMap!!, galleryPhoto)
                     galleryText.setVisibility(View.INVISIBLE)
                     galleryButton.setVisibility(View.INVISIBLE)
                 }
             }
-
-
         }
 
         super.onActivityResult(requestCode, resultCode, data)
@@ -90,5 +93,81 @@ class PhotoActivity : AppCompatActivity(), OnSignDetectedAlert {
 
     override fun onSignDetected(detected:Boolean) {}
 
+    private fun runObjectDetection(bitmap: Bitmap, galleryPhoto: ImageView) {
+        // Step 1: Create TFLite's TensorImage object
+        var processedBitmap = bitmap.copy(Bitmap.Config.ARGB_8888,true)
+        val image = TensorImage.fromBitmap(processedBitmap)
+
+        // Step 2: Initialize the detector object
+        val options = ObjectDetector.ObjectDetectorOptions.builder()
+            .setMaxResults(5)
+            .setScoreThreshold(0.3f)
+            .build()
+        val detector = ObjectDetector.createFromFileAndOptions(
+            this,
+            "mobilenetv1.tflite",
+            options
+        )
+
+        // Step 3: Feed given image to the detector
+        val results = detector.detect(image)
+
+        // Step 4: Parse the detection result and show it
+        val resultToDisplay = results.map {
+            // Get the top-1 category and craft the display text
+            val category = it.categories.first()
+            val text = "${category.label}, ${category.score.times(100).toInt()}%"
+
+            // Create a data object to display the detection result
+            DetectionResult(it.boundingBox, text)
+        }
+        // Draw the detection result on the bitmap and show it.
+        val imgWithResult = drawDetectionResult(bitmap, resultToDisplay)
+        runOnUiThread {
+            galleryPhoto.setImageBitmap(imgWithResult)
+        }
+    }
+
+    private fun drawDetectionResult(
+        bitmap: Bitmap,
+        detectionResults: List<DetectionResult>
+    ): Bitmap {
+        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(outputBitmap)
+        val pen = Paint()
+        pen.textAlign = Paint.Align.LEFT
+
+        detectionResults.forEach {
+            // draw bounding box
+            pen.color = Color.RED
+            pen.strokeWidth = 8F
+            pen.style = Paint.Style.STROKE
+            val box = it.boundingBox
+            canvas.drawRect(box, pen)
+
+
+            val tagSize = Rect(0, 0, 0, 0)
+
+            // calculate the right font size
+            pen.style = Paint.Style.FILL_AND_STROKE
+            pen.color = Color.YELLOW
+            pen.strokeWidth = 2F
+
+            pen.textSize = 96F
+            pen.getTextBounds(it.text, 0, it.text.length, tagSize)
+            val fontSize: Float = pen.textSize * box.width() / tagSize.width()
+
+            // adjust the font size so texts are inside the bounding box
+            if (fontSize < pen.textSize) pen.textSize = fontSize
+
+            var margin = (box.width() - tagSize.width()) / 2.0F
+            if (margin < 0F) margin = 0F
+            canvas.drawText(
+                it.text, box.left + margin,
+                box.top + tagSize.height().times(1F), pen
+            )
+        }
+        return outputBitmap
+    }
 
 }
